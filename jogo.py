d4ee34868b8523641ab216202fbc961b935a50b1
Nery <pedronas@al.insper.edu.br>
2023-04-30 16:56:17 -0300

    redução do código

--- jogo.py
+++ jogo.py
@@ -7,70 +7,27 @@ frota = {}
 while tam >= 1:
     if embarc[tam-1] == 'porta-aviões':
         i = 1
-        while i > 0:
-            print(f'Insira as informações referentes ao navio {embarc[tam-1]} que possui tamanho {tam}')
-            lin = int(input('Linha: '))
-            col = int(input('Coluna: '))
-            orien = int(input('Orientação ([1] Vertical [2] Horizontal): '))
-            if orien == 1:
-                orien = 'vertical'
-            elif orien == 2:
-                orien  = 'horizontal'
-            valido = posicao_valida(frota, lin, col, orien, tam)
-            if valido == True:
-                frota = preenche_frota(frota, embarc[tam-1], lin, col, orien, tam)
-                i -= 1
-            else:
-                print('Esta posição não está válida!')
     elif embarc[tam-1] == 'navio-tanque':
         i = 2
-        while i > 0:
-            print(f'Insira as informações referentes ao navio {embarc[tam-1]} que possui tamanho {tam}')
-            lin = int(input('Linha: '))
-            col = int(input('Coluna: '))
-            orien = int(input('Orientação ([1] Vertical [2] Horizontal): '))
-            if orien == 1:
-                orien = 'vertical'
-            elif orien == 2:
-                orien  = 'horizontal'
-            valido = posicao_valida(frota, lin, col, orien, tam)
-            if valido == True:
-                frota = preenche_frota(frota, embarc[tam-1], lin, col, orien, tam)
-                i -= 1
-            else:
-                print('Esta posição não está válida!')
     elif embarc[tam-1] == 'contratorpedeiro':
         i = 3
-        while i > 0:
-            print(f'Insira as informações referentes ao navio {embarc[tam-1]} que possui tamanho {tam}')
-            lin = int(input('Linha: '))
-            col = int(input('Coluna: '))
+    else:
+        i = 4
+    while i > 0:
+        print(f'Insira as informações referentes ao navio {embarc[tam-1]} que possui tamanho {tam}')
+        lin = int(input('Linha: '))
+        col = int(input('Coluna: '))
+        if embarc[tam-1] != "submarino":
             orien = int(input('Orientação ([1] Vertical [2] Horizontal): '))
             if orien == 1:
                 orien = 'vertical'
             elif orien == 2:
                 orien  = 'horizontal'
-            valido = posicao_valida(frota, lin, col, orien, tam)
-            if valido == True:
-                frota = preenche_frota(frota, embarc[tam-1], lin, col, orien, tam)
-                i -= 1
-            else:
-                print('Esta posição não está válida!')
-    else:
-        i = 4
-        while i > 0:
-            print(f'Insira as informações referentes ao navio {embarc[tam-1]} que possui tamanho {tam}')
-            lin = int(input('Linha: '))
-            col = int(input('Coluna: '))
-            if orien == 1:
-                orien = 'vertical'
-            elif orien == 2:
-                orien  = 'horizontal'
-            valido = posicao_valida(frota, lin, col, orien, tam)
-            if valido == True:
-                frota = preenche_frota(frota, embarc[tam-1], lin, col, orien, tam)
-                i -= 1
-            else:
-                print('Esta posição não está válida!')
+        valido = posicao_valida(frota, lin, col, orien, tam)
+        if valido == True:
+            frota = preenche_frota(frota, embarc[tam-1], lin, col, orien, tam)
+            i -= 1
+        else:
+            print('Esta posição não está válida!')
     tam -= 1
 print(frota)
