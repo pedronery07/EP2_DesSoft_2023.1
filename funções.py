404c4b53adabd2d7a3b60a0ee5a44de7fba3890c
marcelo <marcelocp3@al.insper.edu.br>
2023-05-09 08:17:41 -0300

    Jogadas do jogador

--- funções.py
+++ funções.py
@@ -95,3 +95,16 @@ def posicao_valida(frota,lin,col,orient,tam):
         return False
     else:
         return True
+    
+# Sétima função: Monta tabuleiros
+def monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente):
+    texto = ''
+    texto += '   0  1  2  3  4  5  6  7  8  9         0  1  2  3  4  5  6  7  8  9\n'
+    texto += '__      __\n'
+
+    for linha in range(len(tabuleiro_jogador)):
+        jogador_info = '  '.join([str(item) for item in tabuleiro_jogador[linha]])
+        oponente_info = '  '.join([info if str(info) in 'X-' else '0' for info in tabuleiro_oponente[linha]])
+        texto += f'{linha}| {jogador_info}|     {linha}| {oponente_info}|\n'
+    return texto
+
--- jogo.py
+++ jogo.py
@@ -1,8 +1,34 @@
-from funções import posicao_valida, preenche_frota
+from funções import posicao_valida, preenche_frota, posiciona_frota, afundados, faz_jogada, monta_tabuleiros
+
+resultado = faz_jogada
+print(resultado)
+
+frota_oponente = {
+    'porta-aviões': [
+        [[9, 1], [9, 2], [9, 3], [9, 4]]
+    ],
+    'navio-tanque': [
+        [[6, 0], [6, 1], [6, 2]],
+        [[4, 3], [5, 3], [6, 3]]
+    ],
+    'contratorpedeiro': [
+        [[1, 6], [1, 7]],
+        [[0, 5], [1, 5]],
+        [[3, 6], [3, 7]]
+    ],
+    'submarino': [
+        [[2, 7]],
+        [[0, 6]],
+        [[9, 7]],
+        [[7, 6]]
+    ]
+}
+
+tabuleiro_oponente = posiciona_frota(frota_oponente)
 
 embarc = ['submarino', 'contratorpedeiro', 'navio-tanque', 'porta-aviões']
 tam = 4
-frota = {}
+frota_jogador = {}
 
 while tam >= 1:
     if embarc[tam-1] == 'porta-aviões':
@@ -23,11 +49,46 @@ while tam >= 1:
                 orien = 'vertical'
             elif orien == 2:
                 orien  = 'horizontal'
-        valido = posicao_valida(frota, lin, col, orien, tam)
+        valido = posicao_valida(frota_jogador, lin, col, orien, tam)
         if valido == True:
-            frota = preenche_frota(frota, embarc[tam-1], lin, col, orien, tam)
+            frota_jogador = preenche_frota(frota_jogador, embarc[tam-1], lin, col, orien, tam)
             i -= 1
         else:
             print('Esta posição não está válida!')
     tam -= 1
-print(frota)
+tabuleiro_jogador = posiciona_frota(frota_jogador)
+
+montagem_de_tabuleiros = monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente)
+print(montagem_de_tabuleiros)
+
+#AINDA É NECESSÁRIO IMPRIMIR OS TABULEIROS DO JOGADOR E DO OPONENTE
+# FEITO
+jogando = True
+posicoes_passadas = []
+while jogando:
+    validos = [0,1,2,3,4,5,6,7,8,9]
+    posicao = []
+    linha_ataque = int(input('Linha que deseja atacar: '))
+    while linha_ataque not in validos:
+        print('Linha inválida')
+        linha_ataque = int(input('Linha que deseja atacar: '))
+    posicao.append(linha_ataque)
+    coluna_ataque = int(input("Coluna que deseja atacar: "))
+    while coluna_ataque not in validos:
+        print('Coluna inválida')
+        coluna_ataque = int(input('Coluna que deseja atacar: '))
+    posicao.append(coluna_ataque)
+    if posicao in posicoes_passadas:
+        print(f"A posição linha {linha_ataque} e coluna {coluna_ataque} já foi informada anteriormente!")
+    else:
+        posicoes_passadas.append(posicao)
+        tabuleiro_oponente = faz_jogada(tabuleiro_oponente,linha_ataque,coluna_ataque)
+        montagem_de_tabuleiros = monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente)
+        print(montagem_de_tabuleiros)
+        #AQUI DEVE AINDA OCORRER A VERIFICAÇÃO SE TODAS AS EMBARCAÇÕES DO OPONENTE FORAM AFUNDADAS PARA QUEBRAR OU NÃO O LOOP PRINCIPAL DO JOGO (jogando = False)
+        a = afundados(frota_oponente, tabuleiro_oponente)
+        if a == 10:
+            print('Parabéns! Você derrubou todos os navios do seu oponente!')
+            jogando = False
+        else:
+            continue
